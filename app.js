164c29b652d5e44917e244b08c88c3a91669344c
Bilguun <zorigoobilguun30@gmail.com>
2022-10-04 02:08:26 -0700

    add keyboard

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 let grid = document.getElementById('grid');
-let worldLists = [ 'turuu','ganaa'];
+let keyBoard = document.getElementById("keyBoard");
+keyBoard.className = 'keyboard';
+let worldLists = [ 'table','chair','china','piano','paint'];
 let attempts = [];
 let currentAttempt = '';
 let randomIndex = Math.floor(Math.random() * worldLists.length);
 let secretWord = worldLists[randomIndex];
+let GREY = "#212121";
+let GREEN = "#538d4e";
+let YELLOW = "#b59f3b";
+let LIGHTGREY = "#888" 
 function drawGrid() {
     for (let i = 0; i < 6; i++) {
         let row = document.createElement('div');
@@ -39,21 +45,25 @@ function drawCurrentAttempt(row, attempt) {
     cell.innerText = attempt[i] ?? "";
   }
 }
+
 function getBgColor(attempt,i) {
     let correctLetter = secretWord[i];
     let attemptLetter = attempt[i];
     if (correctLetter === attemptLetter) {
-        return "#538d4e";
+        return GREEN
     } else if (secretWord.indexOf(attemptLetter)===-1|| attemptLetter === undefined) {
-        return ;
+        return GREY;
     } 
-    return "#b59f3b";
+    return YELLOW;
 }
 function handleKeyDown(e) {
     let letter = e.key.toLowerCase();
+    if (e.ctrlKey || e.metaKey || e.altKey) {
+        return;
+    }
     if (letter === 'enter') {
         if (currentAttempt.length < 5) {
-            return
+            return;
         }
         // if (!secretWord.includes(letter)) {
         //     alert('Таны оруулсан үсэг таах үгэнд алга байна')
@@ -69,4 +79,36 @@ function handleKeyDown(e) {
     uptadeGrid()
 }
 
-document.addEventListener('keydown',handleKeyDown)
+document.addEventListener('keydown', handleKeyDown);
+
+function buildKeyboard() {
+    buildKeyboardRow('qwertyuiop',false);
+    buildKeyboardRow("asdfghjkl",false);
+    buildKeyboardRow("zxcvbnm",true);
+}
+buildKeyboard()
+function buildKeyboardRow(letters,isLastRow) {
+    let row = document.createElement('div');
+    if (isLastRow) {
+     let button = document.createElement("button");
+     button.className = "KeyForKeyBoard";
+     button.innerText = 'Enter';
+     button.style.background = LIGHTGREY;
+     row.appendChild(button);
+    }
+    for (let letter of letters) {
+        let button = document.createElement('button');
+        button.className = "KeyForKeyBoard";
+        button.innerText = letter;
+        button.style.background = LIGHTGREY;
+        row.appendChild(button);
+    }
+    if (isLastRow) {
+        let button = document.createElement("button");
+        button.className = "KeyForKeyBoard";
+        button.innerText = "<=";
+        button.style.background = LIGHTGREY;
+        row.appendChild(button);
+    }
+    keyBoard.appendChild(row);
+}
